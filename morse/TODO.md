68d0c5c31c7a43e8cac67f3ac9ce3631a348dc7f
Pierrick Koch <pierrick.koch@gmail.com>
2012-01-19 18:38:25 +0700

    [morse] test explore

--- morse/TODO.md
+++ morse/TODO.md
@@ -7,4 +7,15 @@ TODO
   - Edit beego-robot.blend
   - see: [this slide](http://anr-proteus.github.com/slides/demo.html#slide25) or directly [from youtube](http://www.youtube.com/embed/videoseries?list=PLDC1FC34E5AC69429&hd=1&rel=0)
 
+Docs
+----
+
+# ROS
+
+    sudo apt-get install ros-electric-base ros-electric-bosch-common
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd ..)/explore_beego
+    roslaunch explore_beego explore.launch 
+    rosrun explore_beego remap.py from:=/beego/scan to:=/base_scan
+    # if needed
+    rostopic pub -1 /beego/velocity geometry_msgs/Twist [0.1,0,0] [0,0,0.1]
 
--- morse/beego.py
+++ morse/beego.py
@@ -1,33 +1,40 @@
 from morse.builder import *
+from morse.builder.sensors import *
 
 # Append the robot to the scene
 robot = Robot('beego-robot.blend')
-robot.name = "Beego"
+robot.name = "beego"
 
 # Append an actuator
 motion = Actuator('v_omega')
-motion.name = "Motion"
+motion.name = "velocity"
 motion.translate(z = 0.3)
 robot.append(motion)
 
 # Append a Pose sensor (GPS + Gyroscope)
 pose = Sensor('pose')
-pose.name = "Pose"
+pose.name = "odometry"
 pose.translate(x = -0.12, z = 0.46)
 robot.append(pose)
 
 # Append a sick laser
 sick = Sensor('sick')
-sick.name = "LIDAR"
+sick.name = "scan"
 sick.translate(x = 0.03, z = 0.56)
 robot.append(sick)
 
 # Append a camera
 camera = Sensor('video_camera')
-camera.name = "Camera"
+camera.name = "camera"
 camera.translate(x = 0.15, z = 0.582)
 robot.append(camera)
 camera.properties(cam_width = 128, cam_height = 128, Vertical_Flip = True, capturing = False)
+
+clock = Clock()
+clock.name = "clock"
+clock.properties(middleware_time = True)
+robot.append(clock)
+clock.configure_mw('ros')
 
 # Configure the middlewares
 motion.configure_mw('ros')
@@ -38,6 +45,6 @@ camera.configure_mw('ros')
 # Select the environement
 env = Environment('beego-env.blend')
 env.aim_camera([1.0470, 0, 0.7854])
-# for VirtualBox users:
+# for virtual machine users:
 #env.set_viewport(viewport_shade = 'WIREFRAME')
 
