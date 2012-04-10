f34816c98254f9b9bb8da56ca3498161611fa8d3
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-10 14:45:34 +0700

    On going work on NBVAlgorithm

--- explore_beego/nodes/NextBestViewAlgorithm.py
+++ explore_beego/nodes/NextBestViewAlgorithm.py
@@ -22,22 +22,28 @@ class NextBestViewAlgorithm:
     """Abstract class for NBV algorithms"""
     candidates = None
     bestCandidate = None
+    occupancy_grid = None
 
     def chooseBestCandidate(self):
         abstract # Override me in derived class
         
     def chooseCandidatesOnFrontier(self):
-        shouldBeImplemented
-
+        data = self.occupancy_grid.data
+        height = self.occupancy_grid.info.height
+        width = self.occupancy_grid.info.width
+        candidates = known_perimeter(data, width, height)
+        
     def moveToBestCandidateLocation(self):
+        """Use the navigation stack to move to the goal"""
         shouldBeImplemented
     
     def className(self):
         shouldBeImplemented
 
-    def loop(self, image):
-        self.chooseCandidatesOnFrontier();
-        self.chooseBestCandidate();
+    def loop(self, occupancy_grid):
+        self.occupancy_grid = occupancy_grid
+        self.chooseCandidatesOnFrontier(self)
+        self.chooseBestCandidate()
         self.moveToBestCandidateLocation()
 
     def __init__(self):
