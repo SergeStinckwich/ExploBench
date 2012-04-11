0552994a886dfe43072c6b6aed956cee9f98b3cb
Pierrick Koch <pierrick.koch@gmail.com>
2012-04-11 15:10:02 +0700

    review nbv

--- explore_beego/nodes/NextBestViewAlgorithm.py
+++ explore_beego/nodes/NextBestViewAlgorithm.py
@@ -1,37 +1,60 @@
 #!/usr/bin/env python
 """
 usage:
-rosrun explore_beego NextBestViewAlgorithm.py XXXNBVAlgorithm cmd:=/beego/velocity map:/explore/map
+rosrun explore_beego NextBestViewAlgorithm.py XXXNBVAlgorithm
 """
 
-import roslib
-roslib.load_manifest('rospy')
-roslib.load_manifest('nav_msgs')
-roslib.load_manifest('geometry_msgs')
-roslib.load_manifest('actionlib')
-import rospy
-import nav_msgs
-from geometry_msgs.msg import Twist
 import sys
 import array
 import random
-import actionlib
 import math
+import threading
+from abc import ABCMeta, abstractmethod
+# ROS import
+import roslib
+roslib.load_manifest('rospy')
+roslib.load_manifest('actionlib')
+roslib.load_manifest('nav_msgs')
+roslib.load_manifest('visualization_msgs')
+roslib.load_manifest('move_base')
+import rospy
+import nav_msgs
+import actionlib
+from visualization_msgs.msg import Marker
+from move_base_msgs.msg import MoveBaseAction
 
-from perimeter import known_perimeter
-
-class NextBestViewAlgorithm:
+class NextBestViewAlgorithm(threading.Thread):
     """Abstract class for NBV algorithms"""
-    candidates = None
+    __metaclass__ = ABCMeta
+    # contains the different candidates positions for exploration
+    # dictionnary: id (int32) -> pose (geometry_msgs/Pose)
+    candidates = {}
     bestCandidate = None
-    occupancy_grid = None
     client = None
     pourcentageOfKnowEnv = 0
     maxPourcentageofCoverage = 0.90
 
-    def chooseBestCandidate(self):
-        abstract # Override me in derived class
-    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        rospy.init_node("NBV%s"%self.className)
+        rospy.Subscriber('visualization_marker', Marker, self.handle_markers)
+        self.client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+
+    def run(self):
+        # Waits until the action server has started up and started
+        # listening for goals.
+        self.client.wait_for_server()
+
+        while not rospy.is_shutdown() and \
+              (pourcentageUnkownEnv < maxPourcentageCoverage):
+            self.chooseBestCandidate()
+            self.moveToBestCandidateLocation()
+            rospy.sleep(.2)
+
+        print("exploration done !")
+
+    @abstractmethod
+    def chooseBestCandidate(self): pass
 
     def computePourcentageOfKnownEnv():
         nbOfUnknowCells = 0
@@ -40,65 +63,43 @@ class NextBestViewAlgorithm:
                 nbOfUnknownCells = nbOfUnknowCells + 1
         self.pourcentageOfKnownEnv = 1 - (nbOfUnknownCells / len(data))
 
-    def chooseCandidatesOnFrontier(self):
-        data = self.occupancy_grid.data
-        height = self.occupancy_grid.info.height
-        width = self.occupancy_grid.info.width
-        #Update % of known env vs unknown env
-        self.computePourcentageOfKnownEnv()
-        #Compute candidates on the frontier
-        candidates = known_perimeter(data, width, height)
-        
     def moveToBestCandidateLocation(self):
         """Use the navigation stack to move to the goal"""
 
-        if bestCandidate = None:
+        if not bestCandidate:
             rospy.loginfo('No best candidate')
-        elif:
+        else:
             rospy.loginfo('Move to best candidate')
-            
+
             # Creates a goal to send to the action server.
             goal = MoveBaseGoal()
             goal.target_pose.pose.position.x = bestCandidate[0]
             goal.target_pose.pose.position.y = bestCandidate[1]
-            
+
             # Sends the goal to the action server.
             self.client.send_goal(goal)
-    
+
             # Waits for the server to finish performing the action.
             self.client.waitForResult()
-        
-    def className(self):
-        shouldBeImplemented
 
-    def loop(self, occupancy_grid):
-        #Loop until we cover a large amount of the env
-        if (pourcentageUnkownEnv < maxPourcentageCoverage):
-            self.occupancy_grid = occupancy_grid
-            self.chooseCandidatesOnFrontier(self)
-            self.chooseBestCandidate()
-            self.moveToBestCandidateLocation()
-        
-    def __init__(self):
-        rospy.init_node('NextBestViewAlgorithm:'+self.className)
-        rospy.Subscriber('/map', nav_msgs.msg.OccupancyGrid, self.loop)
-        self.client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
-        
-        # Waits until the action server has started up and started
-        # listening for goals.
-        self.client.wait_for_server()
+    @property
+    def className(self):
+        return self.__class__.__name__
+
+    def handle_markers(self, marker):
+        if marker.action == Marker.DELETE:
+            if marker.id in self.candidates:
+                self.candidates.pop(marker.id)
+        else:
+            self.candidates[marker.id] = marker.pose
 
 class RandomNBVAlgorithm(NextBestViewAlgorithm):
     """Move the robot to a randomly choosen candidate"""
     def chooseBestCandidate(self):
         self.bestCandidate = random.choice(candidates)
-    
-    def className(self):
-        return('RandomNBVAlgorithm')
 
-class MinimumLengthNBVAlgorithm(NextBEstViewAlgorithm):
+class MinimumLengthNBVAlgorithm(NextBestViewAlgorithm):
     """Exploration algorithm that use the criteria of length of the minimum collision-free path to candidate"""
-    
     def distanceBetweenPose(pose1, pose2):
         """Compute the euclidian distance between 2 poses"""
         return sqrt(pow(pose2.position.x-pose1.position.x, 2) + pow(pose2.position.y-pose1.position.y, 2))
@@ -109,7 +110,7 @@ class MinimumLengthNBVAlgorithm(NextBEstViewAlgorithm):
         pathLength = 0
         #Iteration among along the poses in order to compute the length
         for index in range(1, len(poses)):
-            pathLength = pathLength + distanceBetweenPose(poses[index-1], poses[index])
+            pathLength += distanceBetweenPose(poses[index-1], poses[index])
         return pathLength
 
     def chooseBestCandidate(self):
@@ -137,27 +138,21 @@ class MinimumLengthNBVAlgorithm(NextBEstViewAlgorithm):
                 bestCandidate = eachCandidate
                 shortestLength = pathLength
 
-    def className(self):
-        return('MinimumLengthNBVAlgorithm')
-
 class MaxQuantityOfInformationNBVAlgorithm(NextBestViewAlgorithm):
-    def className(self):
-        return('MaxQuantityOfInformationNBVAlgorithm')
     def chhoseBestCandidate(self):
         shouldBeImplemented
 
-class MCDMPrometheeNBVAlgorithm(NextBextViewAlgorithm):
+class MCDMPrometheeNBVAlgorithm(NextBestViewAlgorithm):
     def chooseBestCandidate(self):
         shouldBeImplemented
 
-    def className(self):
-        return('MCDMPrometheeNBVAlgorithm')
-
 def main(argv):
-    rospy.signal_shutdown("MainLoop")
     #Use the Python reflection API to run the suitable NBV class
-    classToLaunch = argv[3]
-    getattr(sys.modules[__name__], classToLaunch)()
+    classToLaunch = argv[1]
+    print(classToLaunch)
+    instance = getattr(sys.modules[__name__], classToLaunch)()
+    instance.start()
+    rospy.spin()
     return 0
 
 if __name__ == "__main__":
