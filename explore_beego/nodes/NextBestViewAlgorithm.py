a29625100614b4932bf5510dfa851ef991a194ec
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-11 16:14:37 +0700

    Function for the quantityOfInformation

--- explore_beego/nodes/NextBestViewAlgorithm.py
+++ explore_beego/nodes/NextBestViewAlgorithm.py
@@ -146,7 +146,7 @@ class MaxQuantityOfInformationNBVAlgorithm(NextBestViewAlgorithm):
     def chhoseBestCandidate(self):
         shouldBeImplemented
     
-    def distance(self, x1,y1,x2,y2):
+    def distance(self, x1, y1, x2, y2):
         return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2))
 
     def quantityOfNewInformation(self, candidate):
