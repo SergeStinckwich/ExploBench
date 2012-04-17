eeddd1d0e2cb40e1fe830411efbb7bf3a53fcc83
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-17 18:59:27 +0700

    Start to work of WFD algorithm

--- explore_beego/nodes/perimeter.py
+++ explore_beego/nodes/perimeter.py
@@ -1,7 +1,8 @@
 import unittest
+import Queue
 
-def known_perimeter(data, width, height):
-    """ get the perimeter of the known area from the occupancy grid
+def build_frontier(data, width, height):
+    """ get the frontier of the known area from the occupancy grid
 
     :param data: The map data, in row-major order, starting with (0,0).
                  Occupancy probabilities are in the range [0,100].
@@ -11,7 +12,7 @@ def known_perimeter(data, width, height):
     :returns: list of point [(x1,y1), (x2,y2), ...]
     """
     p = []
-    def is_unknown_with_known_neighbour(d,w,x,y):
+    def is_unknown_with_known_neighbour(d,w,h,x,y):
         pos = y*w + x
         pos_above = pos - w # (y-1)*w + x
         pos_below = pos + w # (y+1)*w + x
@@ -21,7 +22,8 @@ def known_perimeter(data, width, height):
     # ignore the main border (1 px)
     for x in xrange(1, width-1):
         for y in xrange(1, height-1):
-            if is_unknown_with_known_neighbour(data, width, x, y):
+            pose = (x,y)
+            if is_a_frontier_point(data, width, height, pose):
                 p.append((x, y))
     return p
 
@@ -43,28 +45,69 @@ def test():
     d[center] = 5   # (10,5)
     d[center-1] = 5 # (10,4)
     d[center+1] = 5 # (10,6)
-    p = known_perimeter(d, w, h)
+    p = build_frontier(d, w, h)
     print(p)
     # debug
     for (x,y) in p:
         d[y*w + x] = -2 # debug value "border"
     dump(d,w,h)
 
-class FrontierCandidates(unittest.TestCase):
+def is_a_frontier_point(data, width, height, pose):
+    position = pose[1]*width + pose[0]
+    # Return True if unknown and one of the neightbours is known
+    if (data[position] == -1):
+        pos_above = position - width
+        pos_below = position + width
+        return ( data[position-1] == 0 or data[position+1] == 0 or 
+                 data[pos_above-1] == 0 or data[pos_above] == 0 or data[pos_above+1] == 0 or 
+                 data[pos_below-1] == 0 or data[pos_below] == 0 or data[pos_below+1] == 0)
+    return False
+    
+def wavefront_frontier_detector(data, width, height, pose):
+    map_open_list = 1
+    map_close_list = 2
+    
+    qm = Queue.Queue()
+    qm.put((pose, map_open_list))
+    while !(qm.empty()):
+        p = qm.get()
+        if (p[1] == map_close_list):
+            pass
+        if is_a_frontier_point(data, width, height, p[0]):
+            
+    
+
+class FrontierCandidatesTest(unittest.TestCase):
     def test_no_frontier_when_everything_is_unknown(self):
         width = 50
         height = 30
         data = [-1]*width*height
         expected = []
-        self.assertEquals(expected, known_perimeter(data, width, height))
+        self.assertEquals(expected, build_frontier(data, width, height))
 
     def test_no_frontier_when_everything_is_known(self):
         width = 50
         height = 30
         data = [0]*width*height
         expected = []
-        self.assertEquals(expected, known_perimeter(data, width, height))
-
+        self.assertEquals(expected, build_frontier(data, width, height))
+    
+    def test_known_square_4x4_in_center_6x6_grid_are_frontiers_points(self):
+        width = 6
+        height = 6
+        data = [-1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1,
+                -1, -1,  0,  0, -1, -1,
+                -1, -1,  0,  0, -1, -1,
+                -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1]
+        expected = [(1,1), (1,2), (1,3), (1,4),
+                    (2,1),               (2,4),
+                    (3,1),               (3,4),
+                    (4,1), (4,2), (4,3), (4,4)]
+        for each_pose in expected:
+            self.assertEquals(True, is_a_frontier_point(data, width, height, each_pose))
+                              
     def test_known_square_4x4_in_center_6x6_grid(self):
         width = 6
         height = 6
@@ -78,8 +121,8 @@ class FrontierCandidates(unittest.TestCase):
                     (2,1),               (2,4),
                     (3,1),               (3,4),
                     (4,1), (4,2), (4,3), (4,4)]
-        self.assertEquals(expected, known_perimeter(data, width, height))
-
+        self.assertEquals(expected, build_frontier(data, width, height))
+        
 if __name__ == "__main__":
     unittest.main(exit=False)
     test()
